d8f7f67c399743f9276f5732b6e4239fb6fe1f89
Charley Kevin <charleyk05@gmail.com>
2024-07-23 00:35:04 -0300

    Improved Mobile Version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      keyframes: {
+        pulse: {
+          '0%, 100%': { transform: 'scale(1)', opacity: 1 },
+          '50%': { transform: 'scale(1.02)', opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 }
